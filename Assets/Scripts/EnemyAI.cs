68e8d7bdd4868689ba595638661d0255bc74cc88
Coastal-Wolf <natebluto@gmail.com>
2025-11-10 17:59:36 -0800

    Update_NateScene

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,9 +3,22 @@ using UnityEngine;
 public class script : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private UnityEngine.AI.NavMeshAgent Agent;
+    [SerializeField] private GameObject Follow;
+
     void Start()
     {
-        
+
+        Agent.SetDestination(Follow.transform.position);
+        //playerLocation.position;
+
+
+        //1 Find player position
+        //2 Velocity of object towards position of player
+        //3 Stop when within cetain distance of player
+        //4 IF player greater than that distance of player, repeat step 2
+
     }
 
     // Update is called once per frame
